a55302101fbb2dde8125f66a0eb998b8ed69f52a
IMPP-t <127389961+IMPP-t@users.noreply.github.com>
2023-03-09 11:42:16 +0100

    B01

--- Test01.sol
+++ Test01.sol
@@ -20,6 +20,5 @@ contract SimpleStorageLM2023 {
 }
 
 /** 
-In this repository are my first simple contracts created while learning how to create contracts.
-Generally, this repository was created for my own use, but if someone finds this code useful, feel free to use it.
+B01 repository branch
 */
